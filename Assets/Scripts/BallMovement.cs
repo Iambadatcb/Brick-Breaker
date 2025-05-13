6a9e8ec7b31b4bfbfbb6ce753228f07ddf933361
Iambadatcb <gviduks.cepuliuks@gmail.com>
2025-05-13 17:14:16 +0300

    Bricks

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,31 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BallMovement : MonoBehaviour
 {
-    public float ballSpeed = 3f;
+    public float speed = 5f;         // Speed of the ball
+    public TextMeshProUGUI scoreTxt;
+    
+    public string sceneName;
     private Rigidbody2D rb;
-    private bool hasStarted =false;
-    // Start is called before the first frame update
+    private int score = 0;
+    private static int brickCount;
+
+    
+
+    private bool isLaunched = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity= Vector2.zero;
-    }
+        rb.velocity = Vector2.zero;
+        brickCount = GameObject.FindGameObjectsWithTag("Brick").Length;
+        
 
-    // Update is called once per frame
+    }
     void Update()
     {
-        if(hasStarted!)
-        {
-
-        if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            hasStarted = true;
-            Vector2 initialVelocity = new Vector2(0, ballSpeed);
-            rb.velocity = initialVelocity;
+        if(!isLaunched && Input.GetKeyDown(KeyCode.W)){
+            LaunchBall();
+            isLaunched = true;
         }
+        scoreTxt.text = score.ToString();
+        if(brickCount == 0)
+        {
+            SceneManager.LoadScene(sceneName);
+        }   
+    }
+    void LaunchBall()
+    {
+        
+
+        Vector2 direction = new Vector2(Random.Range(-1f, 1f), 1).normalized;
+        rb.velocity = direction * speed;
+        
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Brick"))
+        {
+            score++;
+            brickCount--;
+            Destroy(collision.gameObject);
+        }
+        // Optionally add logic here for sound effects or other collision responses
+        //hitting bricks
+        // Get current velocity
+        Vector2 velocity = rb.velocity;
+        // Prevent the ball from moving too vertically or horizontally straight,
+        // by slightly tweaking the velocity if angle is too steep or flat.
+        float minXVelocity = 0.5f * speed;
+        if (Mathf.Abs(velocity.x) < minXVelocity)
+        {
+            float newX = velocity.x < 0 ? -minXVelocity : minXVelocity;
+            velocity.x = newX;
+            velocity = velocity.normalized * speed;
+            rb.velocity = velocity;
         }
     }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,28 +9,19 @@ public class Player : MonoBehaviour
     public float speed = 2f;
     public float ballSpeed = 3f;
     private Rigidbody2D rb;
-    private bool hasStarted =false;
+    // private bool hasStarted =false;
     // Start is called before the first frame update
     void Start()
     {
         ball.transform.position = new Vector3(0,(float)-4.1,0);
-        rb = ball.GetComponent<Rigidbody2D>();
-        rb.velocity= Vector2.zero;
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(hasStarted!)
-        {
-
-        if(Input.GetKeyDown(KeyCode.W)||Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            hasStarted = true;
-            Vector2 initialVelocity = new Vector2(0, ballSpeed);
-            rb.velocity = initialVelocity;
-        }
-        }
+        
         if(Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
         {
            player.transform.position = new Vector3(player.transform.position.x - speed*Time.deltaTime, player.transform.position.y, player.transform.position.z); 
